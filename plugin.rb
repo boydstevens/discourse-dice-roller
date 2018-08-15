311be0f284020aedab956c867ad88f3379470326
boydstevens <boyd.stevens@gmail.com>
2018-08-15 14:59:16 -0400

    Added site setting

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,4 @@
 # about: Allows users to roll dice as part of thier post, with protections against editing or modifying results.  
 # version: 0.0.5
 # authors: Arkaal
+enabled_site_setting :dieroll_enabled
