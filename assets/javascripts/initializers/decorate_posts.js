daefc33c1d098fd374509c2ba9e7de8301c03c85
boydstevens <boyd.stevens@gmail.com>
2018-08-15 15:10:38 -0400

    Cleanup Format of Decorator

--- assets/javascripts/initializers/decorate_posts.js
+++ assets/javascripts/initializers/decorate_posts.js
@@ -1,3 +1,16 @@
-api.decorateWidget('post:after', helper => {
-   return helper.h('p.fancy', `I rolled a ${helper.attrs.id} (4D6 - 4).`);
-});
+import { withPluginApi } from 'discourse/lib/plugin-api'
+
+function initializePlugin(api) {
+	/*getCurrentUser() - can display "You rolled a 6" instead of "Arkaal rolled a 6" for logged in user*/
+
+    api.decorateWidget('post:after', helper => {
+	   return helper.h('p.fancy', `I rolled a ${helper.attrs.id} (4D6 - 4).`);
+	});
+}
+
+export default {
+  name: 'retort-button',
+  initialize: function() {
+    withPluginApi('0.8.6', api => initializePlugin(api))
+  }
+}
